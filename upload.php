ecf88a928b233f36b443600216fcd5a5141536f9
jcijb <sengzi@dreamztech.com.my>
2025-06-29 17:23:10 +0800

    feat(upload): add CORS headers to enable cross-origin requests
    The API was previously not accessible from different origins. Added necessary CORS headers to allow POST requests from any origin with Content-Type header.

--- upload.php
+++ upload.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 
 try {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
